2f74054c44874f8ac16dfc98ed86ab756f946a92
S-mishina <seiryu.mishina@zozo.com>
2023-03-12 23:46:42 +0900

    fix

--- p_terraformer/utils/helpers.py
+++ p_terraformer/utils/helpers.py
@@ -5,6 +5,7 @@ import logging
 
 def generate_filename():
     """_summary_
+    A function to get the current time
     """
     dt_now = datetime.datetime.now()
     dt_now = dt_now.strftime("%Y-%m-%d-%H-%M")
@@ -12,9 +13,9 @@ def generate_filename():
 
 def file_handling(dt_now):
     """_summary_
-
+    Convert generated folder to date folder
     Args:
-        dt_now (str): _description_
+        dt_now (str):
     """
     generated_dir = Path("generated")
     generated_dir.rename(dt_now)
@@ -25,5 +26,5 @@ def terraform_init():
     """
     try:
         subprocess.run(["terraform","init"])
-    except subprocess.CalledProcessError:
+    except:
         logging.warning("terraform init did not work!")
